d17a328bd6fb97c98a25590263732390271aebc4
ISAIAS <ISAIAS@LAPTOP-LCA97K57>
2025-08-31 21:41:32 -0600

    EJ_1:pROMEDIO DE ARREGLO

---
+++ EDAT_JD25_P01/EJ1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arreglo = { 4, 6, 7, 9 };
+        int suma = 0;
+        int promedio = 0;
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+           suma=arreglo[i]+suma;
+           promedio=suma/arreglo.Length;
+        }
+        Console.WriteLine("La suma de la matriz es: "+ promedio);
+    }
+}
